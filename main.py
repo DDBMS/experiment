da8b6a2814c488768b060cf47d3ae926bf41f2bd
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-02-18 10:37:23 +0800

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+from faker import Faker
+from Crypto.Hash import *
+import time
+import random
+import string
+
+
+hash = SHA384.new()
+fake = Faker('zh_TW')
+time_arr = []
+for i in range(100):
+    str_test = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10 ** 6))
+
+    x = time.time_ns()
+    hash.update(str_test.encode("utf8"))
+    y = time.time_ns()
+
+    time_arr.append(y-x);
+    print("{} \n> {}\n".format(hash.hexdigest(),y-x))
+
+print((sum(time_arr)/len(time_arr))/(10 ** 9),end="")
+print('秒')
