0f836cafecf90393a3c1d0ad46a5551a83d0341b
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-02-18 13:23:49 +0800

    init

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ for i in range(100):
     hash.update(str_test.encode("utf8"))
     y = time.time_ns()
 
-    time_arr.append(y-x);
+    time_arr.append(y-x)
     print("{} \n> {}\n".format(hash.hexdigest(),y-x))
 
 print((sum(time_arr)/len(time_arr))/(10 ** 9),end="")
