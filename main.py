7beb2e248e2d00ab666b08fbe2e7835cbc2f04ed
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-02-23 00:14:18 +0800

    update

--- main.py
+++ main.py
@@ -27,6 +27,6 @@ print((sum(time_arr) / len(time_arr)) / (10 ** 9), end="")
 print('秒')
 
 plt.plot(time_arr)
-plt.figtext(0.33, 0.02, "Average: {} second / {} items".format((sum(time_arr) / len(time_arr)) / (10 ** 9), len(time_arr)))
+plt.figtext(0.3, 0.02, "Average: {} second / {} items".format((sum(time_arr) / len(time_arr)) / (10 ** 9), len(time_arr)))
 plt.savefig('../fig.png')
 plt.show()
