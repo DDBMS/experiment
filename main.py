e0f165b4468faf96002d6440ddc200b2bfcce122
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-02-22 23:16:50 +0800

    update

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import string
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 # Add a basic title
 plt.title("A 2D histogram")
 
-hash = SHA1.new()
+hash = SHA384.new()
 fake = Faker('zh_TW')
 time_arr = []
-for i in range(100):
-    str_test = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10 ** 6))
+for i in range(50):
+    str_test = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(int(10 ** 5.5)))
 
     x = time.time_ns()
     hash.update(str_test.encode("utf8"))
     y = time.time_ns()
 
-    time_arr.append(y-x)
-    print("{} \n> {}\n".format(hash.hexdigest(),y-x))
+    time_arr.append(y - x)
+    print("{} \n> {}\n".format(hash.hexdigest(), y - x))
 
-print((sum(time_arr)/len(time_arr))/(10 ** 9),end="")
+time_arr = list(filter((0).__ne__, time_arr))
+print((sum(time_arr) / len(time_arr)) / (10 ** 9), end="")
 print('秒')
 
 plt.plot(time_arr)
